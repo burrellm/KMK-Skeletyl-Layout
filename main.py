8450829c1c1cd26fff90c35898f0d8be8099abf2
Michael Burrell <burrellm@email.arizona.edu>
2023-10-10 07:54:55 -0700

    cleaned up main

--- main.py
+++ main.py
@@ -24,11 +24,41 @@ split = Split(
     split_type=SplitType.UART,  # Defaults to UART
     uart_interval=20,  # Sets the uarts delay. Lower numbers draw more power
     data_pin=board.RX,  # The primary data pin to talk to the secondary device with
-    #use_pio=True,  # allows for UART to be used with PIO    
+    use_pio=True,  # allows for UART to be used with PIO    
 )
 side = SplitSide.RIGHT if str(getmount('/').label)[-1] == 'R' else SplitSide.LEFT
 
-keyboard.modules = [layers, split, locks, modtap, holdtap]
+rgb = RGB(pixel_pin=keyboard.rgb_pixel_pin, num_pixels=18, hue_default=135, sat_default=255, val_default=255)
+keyboard.extensions.append(rgb)
+
+# rgb
+class RgbLayers(Layers):
+    last_top_layer = 0
+    hues = [135, 224, 64, 0, 165, 96, 100, 192, 32]
+    wasdglow = [7, 5, 6, 11]
+    numpadglow = [5,6,11,4,7,10,3,8,9]
+    thumbglow = [15, 16, 17]
+    def after_hid_send(self, keyboard):
+        if keyboard.active_layers[0] != self.last_top_layer:
+            self.last_top_layer = keyboard.active_layers[0]
+            rgb.disable_auto_write=True
+            rgb.set_hsv_fill(self.hues[self.last_top_layer], 255, 255)
+
+            # if self.last_top_layer in [2, 3, 4, 5, 6] and side == SplitSide.LEFT:
+            #     for i in self.wasdglow:
+            #         rgb.set_hsv(self.hues[self.last_top_layer + 1], 255, 255, i)
+
+            # if self.last_top_layer in [3, 4, 5] and side == SplitSide.RIGHT:
+            #     for i in self.numpadglow:
+            #         rgb.set_hsv(self.hues[self.last_top_layer + 1], 255, 255, i)
+
+            # if locks.get_caps_lock():
+            #     for i in self.thumbglow:
+            #         rgb.set_hsv(self.hues[self.last_top_layer + 1], 255, 255, i)
+            rgb.show()
+#rgb
+
+keyboard.modules = [layers, split, locks, modtap, holdtap, RgbLayers()]
 
 keyboard.debug_enabled = True
 
@@ -60,8 +90,6 @@ HS_N = KC.HT(KC.N, KC.LSFT, prefer_hold=False, tap_interrupted=True, repeat=Hold
 HS_A = KC.HT(KC.A, KC.LCTL, prefer_hold=False, tap_interrupted=True, repeat=HoldTapRepeat.TAP)
 HS_E = KC.HT(KC.E, KC.LALT, prefer_hold=False, tap_interrupted=True, repeat=HoldTapRepeat.TAP)
 HS_I = KC.HT(KC.I, KC.LGUI, prefer_hold=False, tap_interrupted=True, repeat=HoldTapRepeat.TAP)
-
-
 #key shortcuts
 
 keyboard.keymap = [
@@ -108,38 +136,6 @@ keyboard.keymap = [
                             TB_ESC,  TB_SPC,  TB_TAB,           TB_ENT,  TB_BSPC,  TB_DEL
     ]
 ]
-
-# rgb
-rgb = RGB(pixel_pin=keyboard.rgb_pixel_pin, num_pixels=18, hue_default=135, sat_default=255, val_default=255)
-keyboard.extensions.append(rgb)
-
-class RgbLayers(Layers):
-    last_top_layer = 0
-    hues = (135, 224, 64, 0, 165, 96, 100, 192, 32)
-    wasdglow = [7, 5, 6, 11]
-    numpadglow = [5,6,11,4,7,10,3,8,9]
-    thumbglow = [15, 16, 17]
-    def after_hid_send(self, keyboard):
-        if keyboard.active_layers[0] != self.last_top_layer:
-            self.last_top_layer = keyboard.active_layers[0]
-            rgb.disable_auto_write=True
-            rgb.set_hsv_fill(self.hues[self.last_top_layer], 255, 255)
-
-            # if self.last_top_layer in [2, 3, 4, 5, 6] and side == SplitSide.LEFT:
-            #     for i in self.wasdglow:
-            #         rgb.set_hsv(self.hues[self.last_top_layer + 1], 255, 255, i)
-
-            # if self.last_top_layer in [3, 4, 5] and side == SplitSide.RIGHT:
-            #     for i in self.numpadglow:
-            #         rgb.set_hsv(self.hues[self.last_top_layer + 1], 255, 255, i)
-
-            # if locks.get_caps_lock():
-            #     for i in self.thumbglow:
-            #         rgb.set_hsv(self.hues[self.last_top_layer + 1], 255, 255, i)
-            rgb.show()
-
-keyboard.modules.append(RgbLayers())
-#rgb
 
 if __name__ == '__main__':
     keyboard.go()
